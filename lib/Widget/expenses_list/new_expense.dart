290daeb51ac97b76b5dcfc1e964b269abdbf8d2f
gxtanvir <gxtanvir@gmail.com>
2023-06-11 16:50:50 +0600

    dropdown

--- lib/Widget/expenses_list/new_expense.dart
+++ lib/Widget/expenses_list/new_expense.dart
@@ -19,6 +19,7 @@ class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
   DateTime? selectedDate;
+  Category _selectedCategory = Category.food;
 
   @override
   void dispose() {
@@ -96,6 +97,24 @@ class _NewExpenseState extends State<NewExpense> {
           ),
           Row(
             children: [
+              DropdownButton(
+                  value: _selectedCategory,
+                  items: Category.values
+                      .map(
+                        (category) => DropdownMenuItem(
+                          value: category,
+                          child: Text(category.name.toUpperCase()),
+                        ),
+                      )
+                      .toList(),
+                  onChanged: (value) {
+                    if (value == null) {
+                      return;
+                    }
+                    setState(() {
+                      _selectedCategory = value;
+                    });
+                  }),
               TextButton(
                 onPressed: () {
                   Navigator.pop(context);
